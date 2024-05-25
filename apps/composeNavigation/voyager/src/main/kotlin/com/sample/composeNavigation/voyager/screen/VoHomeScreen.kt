5f0339c3cfa23d0eb158adda8c4fbbbdb82fb247
Dhaval Shah <dhavalshah@gofynd.com>
2024-05-26 01:17:55 +0530

    Add extension functions to pass data back

--- apps/composeNavigation/voyager/src/main/kotlin/com/sample/composeNavigation/voyager/screen/VoHomeScreen.kt
+++ apps/composeNavigation/voyager/src/main/kotlin/com/sample/composeNavigation/voyager/screen/VoHomeScreen.kt
@@ -1,13 +1,16 @@
 package com.sample.composeNavigation.voyager.screen
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
 import com.sample.composeNavigation.common.screen.home.HomeScreenViewModel
 import com.sample.composeNavigation.common.screen.home.HomeScreenWrapper
+import com.sample.composeNavigation.voyager.getResult
 
 data class VoHomeScreen(
     private val token: String
@@ -20,9 +23,18 @@ data class VoHomeScreen(
     override fun Content() {
         val homeViewModel = viewModel<HomeScreenViewModel>()
         val bottomSheetNavigator = LocalBottomSheetNavigator.current
+        val context = LocalContext.current
+
+        val result = bottomSheetNavigator.getResult<String>(screenKey = VoSelectCountryScreen::class.java.name)
 
         LaunchedEffect(key1 = token) {
             homeViewModel.setup(token)
+        }
+
+        LaunchedEffect(key1 = result.value) {
+            if (!result.value.isNullOrBlank()) {
+                Toast.makeText(context, result.value, Toast.LENGTH_SHORT).show()
+            }
         }
 
         HomeScreenWrapper(
--- apps/composeNavigation/voyager/src/main/kotlin/com/sample/composeNavigation/voyager/screen/VoSelectCountryScreen.kt
+++ apps/composeNavigation/voyager/src/main/kotlin/com/sample/composeNavigation/voyager/screen/VoSelectCountryScreen.kt
@@ -4,9 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
-import cafe.adriel.voyager.core.screen.uniqueScreenKey
+import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
 import com.sample.composeNavigation.common.screen.selectCountry.SelectCountryScreenViewModel
 import com.sample.composeNavigation.common.screen.selectCountry.SelectCountryScreenWrapper
+import com.sample.composeNavigation.voyager.hideWithResult
 
 class VoSelectCountryScreen : Screen {
 
@@ -16,11 +17,12 @@ class VoSelectCountryScreen : Screen {
     @Composable
     override fun Content() {
         val viewModel = viewModel<SelectCountryScreenViewModel>()
+        val bottomSheetNavigator = LocalBottomSheetNavigator.current
 
         SelectCountryScreenWrapper(
             viewModel = viewModel,
             onGoBackWithResult = {
-
+                bottomSheetNavigator.hideWithResult(it, key)
             }
         )
     }
